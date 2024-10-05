96aabf6ac03a468514bc167d71affd51c1ae5b68
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-05 19:12:44 +0300

    Created value translator

---
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/services/memory/ValueTranslator.kt
@@ -0,0 +1,28 @@
+package com.cobaltumapps.simplecalculator.v15.calculator.services.memory
+
+
+import android.util.Log
+
+/**
+Класс, преобразующий число типа Double эквивалентному числу  типа Int
+*/
+
+class ValueTranslator {
+    fun translate(value: Double): String {
+        return(
+                try {
+                    if (value % 1 == 0.0) value.toInt()
+                    else value
+                } catch (ex: NumberFormatException) {
+                    Log.d(LOG_TAG, "NumberFormatException: Value can't translated to Int")
+                    value
+                }
+
+                ).toString()
+
+    }
+
+    companion object {
+        const val LOG_TAG = "SC_ValueTranslator"
+    }
+}
