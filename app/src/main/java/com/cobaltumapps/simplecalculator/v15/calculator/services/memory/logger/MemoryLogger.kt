43f0b49b0a9ef77ec1208d75dfe39cf622ce97d0
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-05 17:30:57 +0300

    MemoryLogger.kt moved to package 'logger'

---
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/services/memory/logger/MemoryLogger.kt
@@ -0,0 +1,24 @@
+package com.cobaltumapps.simplecalculator.v15.calculator.services.memory.logger
+
+import android.util.Log
+import com.cobaltumapps.simplecalculator.v15.calculator.services.memory.MemoryController
+
+class MemoryLogger: MemoryController {
+    override fun saveMemoryValue(value: Number, onSuccessful: ((result: Boolean) -> Unit?)) {
+        Log.d(LOG_TAG, "The value: $value has been saved to the memory")
+    }
+
+    override fun readMemory(): Double {
+        Log.d(LOG_TAG, "The memory has been read")
+        return 0.0
+    }
+
+    override fun clearMemory() {
+        Log.d(LOG_TAG, "The memory has been cleared")
+    }
+
+    companion object {
+        const val LOG_TAG = "SC_MemoryControllerTag"
+    }
+
+}
