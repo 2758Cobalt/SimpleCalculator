2ccd90c479f64a41a990e9eda0d11eaa77c77a2e
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-05 17:00:02 +0300

    Configured engineering swiper

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/numpad/engineering/EngineeringSwiper.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/numpad/engineering/EngineeringSwiper.kt
@@ -3,22 +3,28 @@ package com.cobaltumapps.simplecalculator.v15.calculator.numpad.engineering
 import android.view.View
 import androidx.core.view.isVisible
 import com.cobaltumapps.simplecalculator.references.Animations
+import com.cobaltumapps.simplecalculator.v15.constants.Property
+import com.cobaltumapps.simplecalculator.v15.fragments.numpad.interfaces.EngineeringBottomBehaviorListener
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-interface EngineeringAnimator {
-    fun swapEnable(view: View, isEnabled: Boolean, duration: Long = 400L)
-}
-class EngineeringSwiper: EngineeringAnimator {
-    var duration: Long = 400L
+class EngineeringSwiper(
+    private var viewItem: View): EngineeringBottomBehaviorListener
+{
+    private val duration: Long = 400L
 
-    override fun swapEnable(view: View, isEnabled: Boolean, duration: Long) {
-        if (isEnabled){
-            view.isVisible = true
-            Animations.animatePropertyChange(view, "alpha", view.alpha, 1f, duration)
-        }
-        else
-            Animations.animatePropertyChange(view, "alpha", view.alpha, 0f, duration) {
-                view.isVisible = false
+    override fun onStateEngNumpadChanged(bottomSheet: View, newState: Int) {
+        when(newState) {
+            BottomSheetBehavior.STATE_EXPANDED -> {
+                viewItem.isVisible = true
+                Animations.animatePropertyChange(viewItem, Property.alpha.name, viewItem.alpha, 1f, duration)
             }
+
+            BottomSheetBehavior.STATE_COLLAPSED -> {
+                Animations.animatePropertyChange(viewItem, Property.alpha.name, viewItem.alpha, 0f, duration) {
+                    viewItem.isVisible = false
+                }
+            }
+        }
     }
 
 }
