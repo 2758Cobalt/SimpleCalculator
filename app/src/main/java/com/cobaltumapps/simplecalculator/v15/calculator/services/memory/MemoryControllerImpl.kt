913c8faa3dedb5da1e2ec44d1d2c38e181aa6ffb
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-05 19:14:40 +0300

    Configured MemoryControllerImpl and his methods

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/services/memory/MemoryControllerImpl.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/services/memory/MemoryControllerImpl.kt
@@ -1,9 +1,10 @@
 package com.cobaltumapps.simplecalculator.v15.calculator.services.memory
 
 import com.cobaltumapps.simplecalculator.v15.calculator.services.memory.interfaces.MemoryController
+import com.cobaltumapps.simplecalculator.v15.calculator.services.memory.interfaces.MemoryOperationController
 import com.cobaltumapps.simplecalculator.v15.calculator.services.memory.logger.MemoryLogger
 
-class MemoryControllerImpl: MemoryController {
+class MemoryControllerImpl: MemoryController, MemoryOperationController {
     private val memory = Memory()
     private val memoryLogger = MemoryLogger()
 
@@ -21,6 +22,26 @@ class MemoryControllerImpl: MemoryController {
     override fun clearMemory(onSuccessful: ((result: Double) -> Unit?)) {
         memory.clear()
         memoryLogger.clearMemory(onSuccessful)
+        onSuccessful.invoke(memory.read())
+    }
+
+    override fun addToMemory(value: Number, onSuccessful: (result: Double) -> Unit?) {
+        memory.save(memory.read() + value.toDouble())
+        onSuccessful.invoke(memory.read())
+    }
+
+    override fun subtractFromMemory(value: Number, onSuccessful: (result: Double) -> Unit?) {
+        memory.save(memory.read() - value.toDouble())
+        onSuccessful.invoke(memory.read())
+    }
+
+    override fun multiplyToMemory(value: Number, onSuccessful: (result: Double) -> Unit?) {
+        memory.save(memory.read() * value.toDouble())
+        onSuccessful.invoke(memory.read())
+    }
+
+    override fun divideAtMemory(value: Number, onSuccessful: (result: Double) -> Unit?) {
+        memory.save(memory.read() / value.toDouble())
         onSuccessful.invoke(memory.read())
     }
 }
