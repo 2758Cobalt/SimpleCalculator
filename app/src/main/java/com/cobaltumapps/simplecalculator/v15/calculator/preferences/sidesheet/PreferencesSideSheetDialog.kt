d418d018ac32b0a778906f9120b87d3ce80aae6e
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-07 15:36:43 +0300

    Configured the class PreferencesSideSheetDialog.kt

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/preferences/sidesheet/PreferencesSideSheetDialog.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/preferences/sidesheet/PreferencesSideSheetDialog.kt
@@ -1,60 +1,42 @@
 package com.cobaltumapps.simplecalculator.v15.calculator.preferences.sidesheet
 
 import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
-import android.net.Uri
 import android.os.Bundle
 import androidx.core.content.ContextCompat.getString
 import com.cobaltumapps.simplecalculator.R
 import com.cobaltumapps.simplecalculator.databinding.SideSheetPreferencesBinding
-import com.cobaltumapps.simplecalculator.references.ConstantsCalculator
-import com.cobaltumapps.simplecalculator.references.PreferenceKeys
 import com.cobaltumapps.simplecalculator.services.VibratorService
 import com.cobaltumapps.simplecalculator.v15.calculator.preferences.data.PreferencesUserData
 import com.google.android.material.sidesheet.SideSheetDialog
 import com.google.android.material.snackbar.Snackbar
 
-class PreferencesSideSheetDialog(context: Context) : SideSheetDialog(context) {
+class PreferencesSideSheetDialog(context: Context) : SideSheetDialog(context)  {
     private val binding by lazy { SideSheetPreferencesBinding.inflate(layoutInflater) }
 
-    private val sharedPreferences by lazy { context.getSharedPreferences(ConstantsCalculator.vault,Context.MODE_PRIVATE) }
-
-    private val privacyPolicyLink = Intent(Intent.ACTION_VIEW, Uri.parse(ConstantsCalculator.privacyPolicyLink))
+    private val appReloadingSnackBar by lazy { Snackbar.make(binding.root, getString(context, R.string.system_restartMessage), Snackbar.LENGTH_SHORT) }
 
     // Configuration
     var preferencesUserData = PreferencesUserData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        loadConfig()
 
         binding.apply {
             setContentView(root)
 
             // Preferences
-            prefAutoSave.setOnCheckedChangeListener { _, checked ->
-                saveBooleanPreferences(PreferenceKeys.keyMemoryAutoSave, checked)
-                preferencesUserData.memoryAutoSave = checked
-            }
+            prefAutoSave.setOnCheckedChangeListener { _, checked -> preferencesUserData.memoryAutoSave = checked }
 
-            prefKeepLastRecord.setOnCheckedChangeListener { _, checked ->
-                saveBooleanPreferences(PreferenceKeys.keyKeepLastRecord, checked)
-                preferencesUserData.keepLastRecord = checked
-            }
+            prefKeepLastRecord.setOnCheckedChangeListener { _, checked -> preferencesUserData.keepLastRecord = checked }
 
             prefLeftHandMode.setOnCheckedChangeListener { _, checked ->
-                saveBooleanPreferences(PreferenceKeys.keyLeftHandMode, checked)
                 preferencesUserData.leftHandedMode = checked
 
                 if (checked)
-                    Snackbar.make(binding.root, getString(context, R.string.system_restartMessage), Snackbar.LENGTH_SHORT).show()
+                    appReloadingSnackBar.show()
             }
 
-            prefOneHandedMode.setOnCheckedChangeListener { _, checked ->
-                saveBooleanPreferences(PreferenceKeys.keyOneHandedMode, checked)
-                preferencesUserData.oneHandedMode = checked
-            }
+            prefOneHandedMode.setOnCheckedChangeListener { _, checked -> preferencesUserData.oneHandedMode = checked }
 
             prefVibration.setOnCheckedChangeListener { _, checked ->
                 if (checked) {
@@ -63,7 +45,6 @@ class PreferencesSideSheetDialog(context: Context) : SideSheetDialog(context) {
                     vibrator.playVibration(5)
                 }
 
-                saveBooleanPreferences(PreferenceKeys.keyAllowVibration, checked)
                 preferencesUserData.allowVibration = checked
             }
 
@@ -74,39 +55,15 @@ class PreferencesSideSheetDialog(context: Context) : SideSheetDialog(context) {
             prefPrivacyPolicy.setOnClickListener {  }
             prefNews.setOnClickListener { }
 
-            // Releave version
+            // Release version
             val pkgInfo = context.packageManager.getPackageInfo(context.packageName!!,0)
             prefReleaseVersion.text = "${pkgInfo.versionName} (${pkgInfo.versionCode})"
         }
     }
 
-
-    private fun loadConfig() {
-        val newConfig =
-            with(sharedPreferences) {
-                PreferencesUserData(
-                    getBoolean(PreferenceKeys.keyMemoryAutoSave, false),
-                    getBoolean(PreferenceKeys.keyKeepLastRecord, true),
-                    getBoolean(PreferenceKeys.keyLeftHandMode, false),
-                    getBoolean(PreferenceKeys.keyOneHandedMode, false),
-                    getBoolean(PreferenceKeys.keyAllowVibration, true)
-                )
-            }
-
-        this.preferencesUserData = newConfig
-        binding.apply {
-            prefAutoSave.isChecked = newConfig.memoryAutoSave
-            prefKeepLastRecord.isChecked = newConfig.keepLastRecord
-            prefLeftHandMode.isChecked = newConfig.leftHandedMode
-            prefOneHandedMode.isChecked = newConfig.oneHandedMode
-            prefVibration.isChecked = newConfig.allowVibration
-        }
-    }
-
-    private fun saveBooleanPreferences(key: String, variable: Boolean) {
-        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-        editor.putBoolean(key, variable)
-        editor.apply()
+    override fun dismiss() {
+        appReloadingSnackBar.dismiss()
+        super.dismiss()
     }
 }
 
