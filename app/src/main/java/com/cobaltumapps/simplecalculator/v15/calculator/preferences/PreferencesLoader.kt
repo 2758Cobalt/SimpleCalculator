3a6dc1ab96f9ffce837a0f2de6e306558c6a7a92
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-07 14:29:42 +0300

    Created the class PreferencesLoader.kt for DataManagement

---
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/preferences/PreferencesLoader.kt
@@ -0,0 +1,28 @@
+package com.cobaltumapps.simplecalculator.v15.calculator.preferences
+
+import android.content.SharedPreferences
+import com.cobaltumapps.simplecalculator.references.PreferenceKeys
+
+class PreferencesLoader(
+    private val sharedPreferences: SharedPreferences,
+    private val listener: PreferencesLoaderListener? = null): PreferencesLoaderListener {
+
+    override fun loadData(): PreferencesUserData {
+        val loadedConfig =
+        with(sharedPreferences) {
+            PreferencesUserData(
+                getBoolean(PreferenceKeys.keyMemoryAutoSave, false),
+                getBoolean(PreferenceKeys.keyKeepLastRecord, true),
+                getBoolean(PreferenceKeys.keyLeftHandMode, false),
+                getBoolean(PreferenceKeys.keyOneHandedMode, false),
+                getBoolean(PreferenceKeys.keyAllowVibration, true)
+            )
+        }
+        listener?.loadData()
+        return loadedConfig
+    }
+
+    fun loadPreference(key: String, defValue: Boolean): Boolean {
+        return sharedPreferences.getBoolean(key, defValue)
+    }
+}
