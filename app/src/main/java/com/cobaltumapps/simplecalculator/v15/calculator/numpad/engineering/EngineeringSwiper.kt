60f75bb20b0ed0409677c9a431bbe94f8e69a51f
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-15 13:15:59 +0300

    Simplified logic of Engineering swiper

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/numpad/engineering/EngineeringSwiper.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/calculator/numpad/engineering/EngineeringSwiper.kt
@@ -1,30 +1,14 @@
 package com.cobaltumapps.simplecalculator.v15.calculator.numpad.engineering
 
 import android.view.View
-import androidx.core.view.isVisible
-import com.cobaltumapps.simplecalculator.references.Animations
-import com.cobaltumapps.simplecalculator.v15.constants.Property
 import com.cobaltumapps.simplecalculator.v15.fragments.numpad.interfaces.EngineeringBottomBehaviorListener
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class EngineeringSwiper(
-    private var viewItem: View): EngineeringBottomBehaviorListener
-{
-    private val duration: Long = 400L
+class EngineeringSwiper(private var viewItem: View): EngineeringBottomBehaviorListener {
 
     override fun onStateEngNumpadChanged(bottomSheet: View, newState: Int) {
-        when(newState) {
-            BottomSheetBehavior.STATE_EXPANDED -> {
-                viewItem.isVisible = true
-                Animations.animatePropertyChange(viewItem, Property.alpha.name, viewItem.alpha, 1f, duration)
-            }
-
-            BottomSheetBehavior.STATE_COLLAPSED -> {
-                Animations.animatePropertyChange(viewItem, Property.alpha.name, viewItem.alpha, 0f, duration) {
-                    viewItem.isVisible = false
-                }
-            }
-        }
     }
 
+    override fun onSlideEngNumpad(bottomSheet: View, slideOffset: Float) {
+        viewItem.alpha = slideOffset
+    }
 }
