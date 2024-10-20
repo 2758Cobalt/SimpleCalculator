7ad27d6b17728666b4d6326256b839329e8e452d
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-10-21 00:24:50 +0300

    Fix main layout

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/activities/MainCalculatorActivity.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/activities/MainCalculatorActivity.kt
@@ -1,21 +1,15 @@
 package com.cobaltumapps.simplecalculator.v15.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.fragment.app.commit
-import com.cobaltumapps.simplecalculator.R
-import com.cobaltumapps.simplecalculator.activities.ConverterActivity
 import com.cobaltumapps.simplecalculator.databinding.ActivityMainCalculatorBinding
 import com.cobaltumapps.simplecalculator.v15.activities.interfaces.CalculatorNavigationListener
 import com.cobaltumapps.simplecalculator.v15.fragments.calculator.CalculatorFragmentN
-import com.google.android.material.navigation.NavigationView
 
 class MainCalculatorActivity : AppCompatActivity(),
-    CalculatorNavigationListener,
-    NavigationView.OnNavigationItemSelectedListener
+    CalculatorNavigationListener
 {
     private val binding by lazy { ActivityMainCalculatorBinding.inflate(layoutInflater) }
 
@@ -24,42 +18,23 @@ class MainCalculatorActivity : AppCompatActivity(),
         setContentView(binding.root)
 
         window.setBackgroundDrawable(null)
-        binding.mainNavigation.setNavigationItemSelectedListener(this)
 
         val calculatorFragment = supportFragmentManager.findFragmentByTag(CalculatorFragmentN.TAG) as? CalculatorFragmentN
         if (calculatorFragment == null) {
-            // Create new fragment instance and add it
             val newFragment = CalculatorFragmentN()
             supportFragmentManager.commit {
                 replace(binding.placeHolder.id, newFragment, CalculatorFragmentN.TAG)
             }
         } else {
-            // Fragment exists, just restore listener if necessary
             calculatorFragment.setCalculatorNavigationListener(this)
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.main_menu_calculator -> { binding.mainDrawer.close() }
-            R.id.main_menu_converter -> { goConverters() }
-            R.id.main_menu_news -> { goNews() }
-        }
-        return true
-    }
-
     override fun goConverters() {
-        startActivity(
-            Intent(this, ConverterActivity::class.java)
-        )
-    }
-
-    fun goNews() {
-        //startActivity(Intent(this, ConverterActivity::class.java))
-    }
-
-    override fun openNavigationMenu() {
-        binding.mainDrawer.openDrawer(GravityCompat.START)
+        /*supportFragmentManager.commit {
+            replace(binding.placeHolder.id, ConverterCalculatorFragment())
+        }*/
+        Toast.makeText(this, "Go converters", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
