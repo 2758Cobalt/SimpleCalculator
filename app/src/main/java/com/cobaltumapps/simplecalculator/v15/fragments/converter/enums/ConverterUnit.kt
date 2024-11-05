cd549cbaf29e25957caa3f56ff9daf740513c756
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-11-05 13:48:57 +0200

    Added enum ConverterUnit.kt

---
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/fragments/converter/enums/ConverterUnit.kt
@@ -0,0 +1,5 @@
+package com.cobaltumapps.simplecalculator.v15.fragments.converter.enums
+
+enum class ConverterUnit {
+    None, Weight, Length, Time, Speed, Temperature, Volume, Area, Frequency, Data
+}
