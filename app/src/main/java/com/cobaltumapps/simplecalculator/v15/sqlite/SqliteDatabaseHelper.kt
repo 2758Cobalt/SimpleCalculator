3f76259ec8d526bf82775b7008a0c80215c19c0d
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-12-11 16:26:16 +0200

    Added annotation DEPRECATED

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/sqlite/SqliteDatabaseHelper.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/sqlite/SqliteDatabaseHelper.kt
@@ -3,11 +3,11 @@ package com.cobaltumapps.simplecalculator.v15.sqlite
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.util.Log
 import com.cobaltumapps.simplecalculator.v15.calculator.services.history.data.HistoryData
 import com.cobaltumapps.simplecalculator.v15.sqlite.controller.DatabaseController
 import com.cobaltumapps.simplecalculator.v15.sqlite.controller.SqliteDatabaseControllerImpl
 
+@Deprecated("Class child of SQLiteOpenHelper is deprecated")
 class SqliteDatabaseHelper(context: Context?) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION), DatabaseController {
     var dbController = SqliteDatabaseControllerImpl(this)
 
@@ -46,33 +46,6 @@ class SqliteDatabaseHelper(context: Context?) : SQLiteOpenHelper(context, DB_NAM
 
     override fun dropDatabase() {
         dbController.dropDatabase()
-    }
-}
-
-class SQLiteDatabaseControllerLogger: DatabaseController {
-    companion object {
-        const val LOG_TAG = "SC_SQLiteDatabaseControllerTag"
-    }
-
-    override fun insertHistoryData(historyData: HistoryData) {
-        Log.d(LOG_TAG, "${this.javaClass.simpleName}: inserted history data to the database." +
-                "\nExpression: ${historyData.expression}" +
-                "\nResult: ${historyData.result}")
-    }
-
-    override fun updateHistoryData(historyData: HistoryData, idHistory: Int) {
-        Log.d(LOG_TAG, "${this.javaClass.simpleName}: updated by id: $idHistory history data in the database." +
-                "\nExpression: ${historyData.expression}" +
-                "\nResult: ${historyData.result}")
-    }
-
-    override fun getHistoryList(): List<HistoryData> {
-        Log.d(LOG_TAG, "${this.javaClass.simpleName}: obtained history list from the database.")
-        return listOf()
-    }
-
-    override fun dropDatabase() {
-        Log.d(LOG_TAG, "${this.javaClass.simpleName}: the Database has been dropped.")
     }
 }
 
