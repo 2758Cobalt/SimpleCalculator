0cac9b5792d160bc4ec93e31a2b546fd721c4128
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-12-13 22:51:38 +0200

    Renamed a class

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/activities/interfaces/ConverterViewPagerAdapter.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/activities/interfaces/ConverterViewPagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ConverterViewPagerAdapterN(fa: FragmentActivity, private var list: List<Fragment>): FragmentStateAdapter(fa) {
+class ConverterViewPagerAdapter(fa: FragmentActivity, private var list: List<Fragment>): FragmentStateAdapter(fa) {
     override fun getItemCount(): Int {
         return list.size
     }
