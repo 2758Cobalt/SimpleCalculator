38921faf470526cd87c0bc244e878bdfd79036d8
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2024-12-19 16:40:05 +0200

    Refactoring the code

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/sqlite/interfaces/Dao.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/sqlite/interfaces/Dao.kt
@@ -1,6 +1,7 @@
 package com.cobaltumapps.simplecalculator.v15.sqlite.interfaces
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.cobaltumapps.simplecalculator.v15.sqlite.RoomHistoryData
@@ -10,8 +11,11 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface Dao {
     @Insert
-    fun insertItem(item: RoomHistoryData)
+    suspend fun insertItem(vararg historyItem: RoomHistoryData)
 
     @Query("SELECT * FROM ${RoomHistory_TableNames.scHistoryTable}")
     fun getItems(): Flow<List<RoomHistoryData>>
+
+    @Delete
+    suspend fun removeItem(vararg historyItem: RoomHistoryData)
 }
