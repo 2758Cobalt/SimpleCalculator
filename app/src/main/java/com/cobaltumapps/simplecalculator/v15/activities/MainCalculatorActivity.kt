980f8fd30b31fdc39fc09da4c9e4406b2322d782
2758Cobalt <114582793+2758Cobalt@users.noreply.github.com>
2025-01-13 19:56:49 +0200

    Added admob example

--- app/src/main/java/com/cobaltumapps/simplecalculator/v15/activities/MainCalculatorActivity.kt
+++ app/src/main/java/com/cobaltumapps/simplecalculator/v15/activities/MainCalculatorActivity.kt
@@ -2,17 +2,33 @@ package com.cobaltumapps.simplecalculator.v15.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.cobaltumapps.simplecalculator.databinding.ActivityMainCalculatorBinding
 import com.cobaltumapps.simplecalculator.v15.activities.interfaces.CalculatorNavigationListener
 import com.cobaltumapps.simplecalculator.v15.fragments.calculator.CalculatorFragment
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 
 class MainCalculatorActivity : AppCompatActivity(),
     CalculatorNavigationListener
 {
     private val binding by lazy { ActivityMainCalculatorBinding.inflate(layoutInflater) }
 
+    private lateinit var adRequest : AdRequest
+
+    private val handler = Handler()
+    private val delayMillis = 45000L // 20 секунд
+
+    private val requestAdRunnable = object : Runnable {
+        override fun run() {
+            requestAd()
+
+            // Повторно запускаем себя через delayMillis
+            handler.postDelayed(this, delayMillis)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -27,6 +43,30 @@ class MainCalculatorActivity : AppCompatActivity(),
             }
         } else {
             calculatorFragment.setCalculatorNavigationListener(this)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Запрашиваем рекламу
+        requestAd()
+
+        // Планируем выполнение запроса рекламы каждые 20 секунд
+        handler.postDelayed(requestAdRunnable, delayMillis)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(requestAdRunnable)
+    }
+
+    private fun requestAd() {
+        adRequest = AdRequest.Builder().build()
+        MobileAds.initialize(this) {
+            binding.adViewBanner.loadAd(adRequest)
+
         }
     }
 
